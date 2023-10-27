e8fbde737aab51e1b9aede3b764d08caff9170b9
Uzair Shahid <uzairu809@gmail.com>
2023-10-27 18:06:15 +0500

    addItem button function done

--- src/GuiMainForm.java
+++ src/GuiMainForm.java
@@ -3,7 +3,10 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+//TODO: Make Table non edit table
 public class GuiMainForm {
    JFrame jFrame;
    DefaultTableModel booksTableModel;
@@ -16,6 +19,57 @@ public class GuiMainForm {
       jFrame.setLayout(new BorderLayout());
 
       JButton b1 = new JButton("Add Item");
+      b1.addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            Dialog addItemDialog= new Dialog(jFrame);
+            addItemDialog.setVisible(true);
+            addItemDialog.setSize(500,150);
+            addItemDialog.setLayout(new BorderLayout());
+
+
+            JButton done=new JButton("Done");
+
+            JLabel title=new JLabel("Title:");
+            JTextField titleTextField=new JTextField(20);
+
+            JTextField authorTextField=new JTextField(20);
+            JLabel author=new JLabel("Author:");
+
+            JLabel publicationYear=new JLabel("Publication Year:");
+            JTextField publicationTextField=new JTextField(20);
+
+            JPanel panel=new JPanel(new GridLayout(4,3));
+            panel.add(title);
+            panel.add(titleTextField);
+
+            panel.add(author);
+            panel.add(authorTextField);
+
+            panel.add(publicationYear);
+            panel.add(publicationTextField);
+
+            addItemDialog.add(panel,BorderLayout.NORTH);
+            addItemDialog.add(done,BorderLayout.SOUTH);
+
+            done.addActionListener(new ActionListener() {
+               @Override
+               public void actionPerformed(ActionEvent e) {
+                  addItemDialog.dispose();
+                  String title=titleTextField.getText();
+                  String author=authorTextField.getText();
+                  int year=Integer.parseInt(publicationTextField.getText());
+                  Book book=new Book(title,0,author,year);
+                  addRow(book);
+                  //check if all cells are full and save data
+               }
+            });
+
+
+
+         }
+      });
+
       JButton b2 = new JButton("Edit Item");
       JButton b3 = new JButton("Delete Item");
       JButton b4 =new JButton("Popularity Count");
@@ -38,6 +92,7 @@ public class GuiMainForm {
       JScrollPane scrollPaneTable=new JScrollPane(booksShelf);
       booksShelf.setSize(700,30);
       jFrame.add(scrollPaneTable,BorderLayout.NORTH);
+
 
 
    }
